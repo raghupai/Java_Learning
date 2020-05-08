b94fa357b98e134bb7e3d81d7380ef902ef5d406
raghuveer <raghuveer@192.168.0.100>
2020-05-08 23:13:15 +0530

    Deleting Project

--- Java_Learning/src/classObject/ABC.java
+++
@@ -1,7 +0,0 @@
-package classObject;
-
-public class ABC{
-	private static void main(String args[]){
-		System.out.println("Hello World");
-	}
-}
--- Java_Learning/src/classObject/ABCBank.java
+++
@@ -1,9 +0,0 @@
-package classObject;
-
-class ABCBank{
-	int phone;
-	double getPhone(){
-		System.out.println("phone number received");
-		return phone;
-	}
-}
